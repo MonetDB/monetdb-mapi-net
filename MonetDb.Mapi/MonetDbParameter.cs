71b3888bd3a8be1441a536f52ad4a71449a96798
RTLABS\ilya.khaybullin <ilya.khaybullin@rtlabs.ru>
2019-05-14 11:05:50 +0300

    формат числа с плавающей точкой

--- MonetDb.Mapi/MonetDbParameter.cs
+++ MonetDb.Mapi/MonetDbParameter.cs
@@ -3,9 +3,39 @@
     using System;
     using System.Data;
     using System.Data.Common;
+    using System.Globalization;
 
     internal class MonetDbParameter : DbParameter
     {
+        private static readonly NumberFormatInfo numberFormatInfo = new NumberFormatInfo
+        {
+            CurrencyDecimalDigits = NumberFormatInfo.InvariantInfo.CurrencyDecimalDigits,
+            CurrencyDecimalSeparator = NumberFormatInfo.InvariantInfo.CurrencyDecimalSeparator,
+            CurrencyGroupSeparator = NumberFormatInfo.InvariantInfo.CurrencyGroupSeparator,
+            CurrencyGroupSizes = NumberFormatInfo.InvariantInfo.CurrencyGroupSizes,
+            CurrencyNegativePattern = NumberFormatInfo.InvariantInfo.CurrencyNegativePattern,
+            CurrencyPositivePattern = NumberFormatInfo.InvariantInfo.CurrencyPositivePattern,
+            CurrencySymbol = NumberFormatInfo.InvariantInfo.CurrencySymbol,
+            NaNSymbol = NumberFormatInfo.InvariantInfo.NaNSymbol,
+            NegativeInfinitySymbol = NumberFormatInfo.InvariantInfo.NegativeInfinitySymbol,
+            NegativeSign = NumberFormatInfo.InvariantInfo.NegativeSign,
+            NumberDecimalDigits = NumberFormatInfo.InvariantInfo.NumberDecimalDigits,
+            NumberDecimalSeparator = ".",
+            NumberGroupSeparator = NumberFormatInfo.InvariantInfo.NumberGroupSeparator,
+            NumberGroupSizes = NumberFormatInfo.InvariantInfo.NumberGroupSizes,
+            NumberNegativePattern = NumberFormatInfo.InvariantInfo.NumberNegativePattern,
+            PercentDecimalDigits = NumberFormatInfo.InvariantInfo.PercentDecimalDigits,
+            PercentDecimalSeparator = ".",
+            PercentGroupSeparator = NumberFormatInfo.InvariantInfo.PercentGroupSeparator,
+            PercentGroupSizes = NumberFormatInfo.InvariantInfo.PercentGroupSizes,
+            PercentNegativePattern = NumberFormatInfo.InvariantInfo.PercentNegativePattern,
+            PercentPositivePattern = NumberFormatInfo.InvariantInfo.PercentPositivePattern,
+            PercentSymbol = NumberFormatInfo.InvariantInfo.PercentSymbol,
+            PerMilleSymbol = NumberFormatInfo.InvariantInfo.PerMilleSymbol,
+            PositiveInfinitySymbol = NumberFormatInfo.InvariantInfo.PositiveInfinitySymbol,
+            PositiveSign = NumberFormatInfo.InvariantInfo.PositiveSign,
+        };
+
         internal string GetProperParameter()
         {
             if (this.Value == DBNull.Value)
@@ -21,6 +51,10 @@
             else if (this.Value is DateTime dateTime)
             {
                 return "'" + dateTime.ToString("yyyy-MM-dd HH:mm:ss") + "'";
+            }
+            else if (this.Value is double doubleVal)
+            {
+                return doubleVal.ToString("G17", numberFormatInfo);
             }
 
             return this.Value.ToString();
