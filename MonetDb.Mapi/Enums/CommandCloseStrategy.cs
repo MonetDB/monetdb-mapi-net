6c21c63773c614cc65172ad60614940135e3e2c5
MitchellWeg <mitchell.w@live.nl>
2021-01-19 15:06:26 +0100

    add xml comments for enums

--- MonetDb.Mapi/Enums/CommandCloseStrategy.cs
+++ MonetDb.Mapi/Enums/CommandCloseStrategy.cs
@@ -1,9 +1,18 @@
 ﻿namespace MonetDb.Mapi.Enums
 {
+    /// <summary>
+    /// Enum definitions for the close Commands
+    /// </summary>
     public enum CommandCloseStrategy
     {
+        /// <summary>
+        /// None
+        /// </summary>
         None = 0,
 
+        /// <summary>
+        /// Terminated Session
+        /// </summary>
         TerminateSession
     }
 }
